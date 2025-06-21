2348f5443f979ccfd96ed4a1a28ad5572819aa33
LuK1337 <priv.luk@gmail.com>
2025-06-21 13:44:53 +0200

    iceland: Enable QTI recovery minui backend
    Change-Id: Ia6cd1edeb5f4b1512a1345b1fec51c11129ed068

--- device.mk
+++ device.mk
@@ -55,6 +55,9 @@ PRODUCT_PACKAGES += \
 # Power
 $(call soong_config_set,qtipower,tap_to_wake_node,/proc/touchpanel/double_tap_enable)
 
+# Recovery
+$(call soong_config_set_bool,recovery,target_recovery_uses_qti_drm,true)
+
 # Regional properties
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/vendor/odm/etc/25927/build.default.prop:$(TARGET_COPY_OUT_ODM)/etc/25927/build.default.prop \
